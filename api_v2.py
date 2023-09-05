794f00e40af892b0e58157c89718cfe854089abe
doi@env.cs.i.nagoya-u.ac.jp <doi@env.cs.i.nagoya-u.ac.jp>
2023-09-05 20:09:50 +0900

    commit

--- api_v2.py
+++ api_v2.py
@@ -12,7 +12,7 @@ router = APIRouter()
 
 
 @router.get("/api/v2/latest_24h")
-def read_latest_24h_data(data: str = 'multi_env_sensor', format: str = 'csv'):
+async def read_latest_24h_data(data: str = 'multi_env_sensor', format: str = 'csv'):
 
     if (data == 'multi_env_sensor'):
 
@@ -84,4 +84,77 @@ def read_latest_24h_data(data: str = 'multi_env_sensor', format: str = 'csv'):
 
     else:
         return "Invalid data specified."
-    
+
+
+@router.get("/api/v2/")
+async def read_data(data: str = 'multi_env_sensor', format: str = 'csv', start_date: str = None, end_date: str = None):
+
+    if (start_date == None or end_date == None):
+        return "No start date specified."
+
+    if (data == 'multi_env_sensor'):
+
+        # connect to database
+        conn = sqlite3.connect('../env_data.db')
+        c = conn.cursor()
+
+        # calc end date + 1 day
+        end_date = datetime.strptime(end_date, '%Y-%m-%d')
+        end_date = end_date + timedelta(days=1)
+        end_date = end_date.strftime('%Y-%m-%d')
+
+        # get latest 24h data from database
+        c.execute("SELECT timestamp, temperature, humidity, pressure, uv_index, illuminance, altitude FROM measurements WHERE ? <= timestamp and timestamp <= ?" , (start_date, end_date,))
+        data = c.fetchall()
+
+        # close database connection
+        conn.close()
+
+        # split data into lists
+        timestamps = [row[0] for row in data]
+        temperatures = [row[1] if row[1] > -50 else np.nan for row in data]
+        humidity = [row[2] for row in data]
+        pressure = [row[3] for row in data]
+        uv_intensity = [row[4] if row[4] > 0 else 0 for row in data]
+        lx_intensity = [row[5] for row in data]
+
+
+        # return data in specified format
+        if (format == 'csv'):
+
+            stream = io.StringIO()
+
+            writer = csv.writer(stream)
+
+            writer.writerow(['timestamp', 'temperature', 'humidity', 'pressure', 'uv intensity', 'luminous intensity'])
+
+            for ts, temp, hum, pres, uv, lx in zip(timestamps, temperatures, humidity, pressure, uv_intensity, lx_intensity):
+                writer.writerow([ts, temp, hum, pres, uv, lx])
+
+            return StreamingResponse(iter([stream.getvalue()]), media_type="text/csv")
+        
+        elif (format == 'json'):
+
+            data = []
+
+            for ts, temp, hum, pres, uv, lx in zip(timestamps, temperatures, humidity, pressure, uv_intensity, lx_intensity):
+                entry = {
+                    "timestamp": ts,
+                    "temperature": temp,
+                    "humidity": hum,
+                    "pressure": pres,
+                    "uv intensity": uv,
+                    "luminous intensity": lx
+                }
+                data.append(entry)
+                    
+            # convert data to json
+            json_data = json.dumps(data, indent=4)
+
+            return JSONResponse(json_data)
+
+        else:
+            return "Invalid format specified. Valid formats are 'csv' and 'json'."
+
+    else:
+        return "Invalid data specified."
